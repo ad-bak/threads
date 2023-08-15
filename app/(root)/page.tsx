821a07568c6b50bdc0033fe860fb818867785501
Adnan <adbak.dev@gmail.com>
2023-08-15 03:35:51 +0200

    started threadcard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,34 @@
-import { UserButton } from "@clerk/nextjs";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  const result = await fetchPosts(1, 30);
+  console.log(result);
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="text-center">No posts yet</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post.id}
+                id={post.id}
+                currentUser={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
