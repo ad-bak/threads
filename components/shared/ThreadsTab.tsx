5c4ad48b3910a389d5a674ddc308326af9edf2e6
Adnan <adbak.dev@gmail.com>
2023-08-19 22:58:13 +0200

    profile

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+const ThreadsTab = ({ currentUserId, accountId, accountType }: Props) => {
+  return <div> adas</div>;
+};
+
+export default ThreadsTab;
